487368b20eace7f4cb42aa9f79ab1d2acfc82b1d
Ruriel Nunes <103866609+RurielSNunes@users.noreply.github.com>
2024-02-19 23:54:52 -0300

    Coreção Ex 002
    Correção ortografica.

--- Ex002.py
+++ Ex002.py
@@ -1,4 +1,4 @@
-"""Ex 002 - FAça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas."""
+"""Ex 002 - Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas."""
 
 nome = input("Digite seu nome?")
 print('E um prazer te conhecer, {}!'.format(nome))
