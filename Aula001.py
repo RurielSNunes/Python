6a8c444d2c9aff300435f9d00591785774a3c48f
Ruriel Nunes <103866609+RurielSNunes@users.noreply.github.com>
2024-02-20 00:47:30 -0300

    Criação da Aula001
    Primeiros Comandos em Python

---
+++ Aula001.py
@@ -0,0 +1,27 @@
+"""Aula 001 - Primeiros comandos em Python"""
+
+"As aspas simples ou dupla são delimitadores para mensagens"
+"Toda função tem que ter parenteses"
+"Números são usados primordialmente para fazer cálculos e não precisam ficar entre aspas"
+'Diferença entre uma mensagem e uma soma'
+
+print('Olá, Mundo!')
+
+print(7+5)
+"Mostra o resultado"
+print('7+5')
+"Mostra o que está entre as aspas"
+print('7'+'5')
+print('7','5')
+"Mostra a junção dos dois números como mensagem, e pode ser usado o sinal de mais (+) ou a vírgula (,)"
+
+"""Prática"""
+"As variáveis devem sempre ser escritas com letras minúsculas"
+"Objeto e mais que uma variável"
+"O sinal de igual depois da variável significa (recebe)"
+
+nome = 'Ruriel'
+idade = '20'
+peso = '85.5'
+print(nome, idade, peso)
+"As variáveis sõe espaços na memória guardados no computador"
