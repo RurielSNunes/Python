17bf419cc8d099923ef5feee01ab01ea111e3fae
Ruriel Nunes <103866609+RurielSNunes@users.noreply.github.com>
2024-02-20 21:57:54 -0300

    Adicionei os desafios
    Desafio 1 2 3

--- Aula004.py
+++ Aula004.py
@@ -25,3 +25,28 @@ idade = '20'
 peso = '85.5'
 print(nome, idade, peso)
 "As variáveis sõe espaços na memória guardados no computador"
+
+nome = input('Qual é o seu nome?')
+idade = input('Qual é a sua idade?')
+peso = input('Qual é o seu peso?')
+print(nome, idade, peso)
+
+"""Desafio"""
+'01 - Crie um script Python que leia o nome de uma pessoa e mostre uma mensagemde boas-vindas de acordo com o valor digitado.'
+'Minha resposta'
+nome = input('Qual é o seu nome?')
+print('Seja bem vindo(a),{} !'.format(nome))
+
+'02 - Crie um scrippt Python que leia o dia, o m~es e o ano de uma pessoa e mostre uma mensagem com a data formatada.'
+'Minha resposta'
+dia = input('Qual o dia que você nasceu?')
+mes = input('Qual o mês você nasceu?')
+ano = input('Qual e o ano que você nasceu?')
+print('Você nasceu no dia , {} de {} de {} . Correto?'.format(dia, mes, ano))
+
+'03 - Crie um script Python que leia dois números e tente mostrar a soma entre eles.'
+'Minha respota'
+n1 = int(input('Primeiro número:'))
+n2 = int(input('Segundo número:'))
+soma = (n1 + n2)
+print('A soma entre {}+{}={}.'.format(n1, n2, soma))
