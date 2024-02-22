f93e4635e997e10eb118646ed7ef683189f18a9d
Ruriel Nunes <103866609+RurielSNunes@users.noreply.github.com>
2024-02-21 21:35:18 -0300

    Criação da Aula 006
    Tipos Primitivos e Saída de Dados

---
+++ Aula006.py
@@ -0,0 +1,10 @@
+# Aula 006 - Tipos Primitivos e saída de Dados
+
+'Inteiro - int ( 7, -4, 0, 9875)'
+'Flutuante - float (4.5, 0.076, -15.223, 7.0)'
+'Boleano - bool (True, False)'
+'Stringue - str ('Olá', '7.5', '')'
+
+#print('A soma vale', s)
+#print('A soma vale {}'.format(s))
+'Metodo format na str'
