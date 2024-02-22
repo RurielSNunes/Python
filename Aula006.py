c4cd2edd2a06c222ab660755f2fe5c3774280a79
Ruriel Nunes <103866609+RurielSNunes@users.noreply.github.com>
2024-02-21 22:10:15 -0300

    Atualização

--- Aula006.py
+++ Aula006.py
@@ -1,10 +1,15 @@
 # Aula 006 - Tipos Primitivos e saída de Dados
 
-'Inteiro - int ( 7, -4, 0, 9875)'
-'Flutuante - float (4.5, 0.076, -15.223, 7.0)'
-'Boleano - bool (True, False)'
-'Stringue - str ('Olá', '7.5', '')'
+#'Inteiro - int ( 7, -4, 0, 9875)'
+#'Flutuante - float (4.5, 0.076, -15.223, 7.0)'
+#'Boleano - bool (True, False)'
+#'Stringue - str ('Olá', '7.5', '')'
 
 #print('A soma vale', s)
 #print('A soma vale {}'.format(s))
-'Metodo format na str'
+#'Metodo format na str'
+
+#Pratica
+
+n = input('Digite um valor: ')
+print(type(n))
