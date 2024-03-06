1d13f6af741f747446375024b34933ae0883c603
Ruriel Nunes <103866609+RurielSNunes@users.noreply.github.com>
2024-03-05 23:23:46 -0300

    add

--- Aula006.py
+++ Aula006.py
@@ -17,12 +17,17 @@
 #n = int(input('Digite um valor: '))
 #print(type(n))
 
+#==========================
+
 #n1 = int(input('Digite um valor: '))
 #n2 = int(input('Digite outro valor: '))
 #s = n1 + n2
 #print('A soma vale', s)
 
-n1 = int(input('Digite um valor: '))
-n2 = int(input('Digite outro valor: '))
-s = n1 + n2
-print('A soma entre {} e {} vale {}'.format(n1 , n2, s))
+#n1 = int(input('Digite um valor: '))
+#n2 = int(input('Digite outro valor: '))
+#s = n1 + n2
+#print('A soma entre {} e {} vale {}'.format(n1 , n2, s))
+
+#===========================
+
