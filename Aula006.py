7be325acb6de2aacee29ba984b93c5c66addf92f
Ruriel Nunes <103866609+RurielSNunes@users.noreply.github.com>
2024-03-05 21:29:36 -0300

    Adição na Aula 006

--- Aula006.py
+++ Aula006.py
@@ -9,7 +9,20 @@
 #print('A soma vale {}'.format(s))
 #'Metodo format na str'
 
-#Pratica
+#Prática
 
-n = input('Digite um valor: ')
-print(type(n))
+#n = input('Digite um valor: ')
+#print(type(n))
+
+#n = int(input('Digite um valor: '))
+#print(type(n))
+
+#n1 = int(input('Digite um valor: '))
+#n2 = int(input('Digite outro valor: '))
+#s = n1 + n2
+#print('A soma vale', s)
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+print('A soma entre {} e {} vale {}'.format(n1 , n2, s))
